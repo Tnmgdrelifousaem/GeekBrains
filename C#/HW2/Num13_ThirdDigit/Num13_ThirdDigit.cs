e37f8f54d1e890b56ab3fa02f7eff43a5c0c0cde
Lazert <Lazert278@yandex.ru>
2023-10-15 15:28:35 +0300

    first commit

---
+++ C#/HW2/Num13_ThirdDigit/Num13_ThirdDigit.cs
@@ -0,0 +1,16 @@
+﻿
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+if (number >= 100)
+{
+    while (number > 999)
+    {
+        number = number / 10;
+    }
+    int result = number % 10;
+    Console.WriteLine($"Третья цифра: {result}");
+}
+else
+{
+    Console.WriteLine("Третьей цифры нет");
+}
---
+++ C#/HW2/Num15_DayNumber/Num15_DayNumber.cs
@@ -0,0 +1,18 @@
+﻿    Console.Write("Введите номер дня недели (1-7): ");
+    int dayNumber = int.Parse(Console.ReadLine());
+
+    if (dayNumber >= 1 && dayNumber <= 7)
+    {
+        if (dayNumber == 6 || dayNumber == 7)
+        {
+            Console.WriteLine("День является выходным");
+        }
+        else
+        {
+            Console.WriteLine("День не является выходным");
+        }
+    }
+    else
+    {
+        Console.WriteLine("Некорректный номер дня недели");
+    }
