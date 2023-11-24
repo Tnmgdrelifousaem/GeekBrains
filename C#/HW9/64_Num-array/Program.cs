37213a2f393d14efbfa0eb7b39b05a147d04b646
Lazert <Lazert278@yandex.ru>
2023-11-25 01:01:30 +0300

    Added HW9

---
+++ C#/HW9/64_Num-array/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введите значение N: ");
+        int n = int.Parse(Console.ReadLine());
+
+        PrintNumbers(n);
+
+        Console.ReadKey();
+    }
+
+    static void PrintNumbers(int n)
+    {
+        Console.Write(n + " ");
+
+        if (n > 1)
+            PrintNumbers(n - 1);
+    }
+}
---
+++ C#/HW9/66_N-Sum/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введите значение M: ");
+        int m = int.Parse(Console.ReadLine());
+
+        Console.Write("Введите значение N: ");
+        int n = int.Parse(Console.ReadLine());
+
+        int sum = SumNumbers(m, n);
+
+        Console.WriteLine("Сумма натуральных чисел в промежутке от {0} до {1} равна {2}", m, n, sum);
+
+        Console.ReadKey();
+    }
+
+    static int SumNumbers(int m, int n)
+    {
+        if (m == n)
+            return m;
+        else if (m < n)
+            return m + SumNumbers(m + 1, n);
+        else
+            return n + SumNumbers(n + 1, m);
+    }
+}
